9a3fe8183c64fb670344ba41dd70dcdf6f70bd75
mherrerabarraza <pcserviceschile@gmail.com>
2024-10-22 12:29:05 +0100

    modified function for more comprehensive results

--- app/insert_vectors.py
+++ app/insert_vectors.py
@@ -11,13 +11,19 @@ vec = VectorStore()
 # Is it asssumed that this data was proccessed in a previous steps
 # where the info from the magazine was extracted and stored in a CSV file
 
-magazine_content_df = pd.read_csv("../data/obtained_query.csv", sep=";")
+magazine_content_df = pd.read_csv("../data/random_books_data.csv", sep=";")
 
 # Prepare data for insertion
 # prepared for using pgvector
 def prepare_record(row):
     # Create a content string for the keyword search index
-    content = f"Title: {row['title']}\nAuthor: {row['author']}\nContent: {row['content']}"
+    content = (
+        f"title: {row['title']}\n"
+        f"author: {row['author']}\n"
+        f"publication_date: {row['publication_date']}\n"
+        f"category: {row['category']}\n"
+        f"content: {row['content']}"
+    )
     # The generated vector embedding for vector search
     # https://timescale.github.io/python-vector/tsv_python_getting_started_tutorial.html
     embedding = vec.get_embedding(content)
